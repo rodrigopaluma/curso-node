137fc1d00e71a07d8d001b5c7c5cfeeca253b87f
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-07-06 19:43:49 -0300

    Fundamnetos: Modulos externos.

---
+++ fundamentos/ler-argumentos/index.js
@@ -0,0 +1,13 @@
+// Nome
+
+console.log(process.argv);
+
+const args = process.argv.slice(2);
+
+console.log(args);
+
+const nome = args[0].split('=')[1];
+const idade = args[1].split('=')[1];
+
+console.log(nome, idade);
+console.log(`Olá ${nome}, você tem ${idade} anos.`);
---
+++ fundamentos/modulos/2_export_import/index.mjs
@@ -0,0 +1,4 @@
+import soma from './meu-modulo.mjs'
+
+soma(5,6);
+soma(40,36);
