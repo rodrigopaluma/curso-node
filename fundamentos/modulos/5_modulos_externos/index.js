7b0c6c45e897c472fdbca51917b0bcc97404f67e
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-07-07 06:33:04 -0300

    Trabalhando com módulos externos e incluindo gitgonore.

--- fundamentos/modulos/5_modulos_externos/index.js
+++ fundamentos/modulos/5_modulos_externos/index.js
@@ -3,3 +3,11 @@ const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
 
 console.log(args);
+
+const nome = args["nome"];
+
+console.log(`Olá ${nome}`);
+
+const profissao = args["profissao"];
+
+console.log(nome, profissao);
